42f5cbe4098a95083c75b60446c3872824cc4b59
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-20 13:53:44 -0400

    first commit

---
+++ 1_funcion.js
@@ -0,0 +1,16 @@
+// function example(a, b, c){
+//  return a+b+c
+//  }
+// console.log(example(1, 2, 3));
+
+const example1 = function sumar(a, b, c) {    
+ return a + b + c;
+};
+
+console.log(example1(1, 2, 3));
+
+const example2 = function(a, b, c) {   
+ return a + b + c;
+};
+
+console.log(example2(1, 2, 3));
