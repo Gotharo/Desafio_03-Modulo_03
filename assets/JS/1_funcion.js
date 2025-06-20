bee535384d01a1ff79dd0c15549ebc0d8becf784
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-20 15:11:46 -0400

    feature: se termina el ejercicio 2_arrow

---
+++ assets/JS/1_funcion.js
@@ -0,0 +1,13 @@
+// function example(a, b, c){
+//  return a+b+c
+//  }
+// console.log(example(1, 2, 3));
+
+
+
+
+const example1 = function(a, b, c) {   
+ return a + b + c;
+};
+
+console.log(example1(3, 4, 5));
