f65b37a9cbd74b586a44ff0cd02fc37cf6491895
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-22 16:23:39 -0400

    feature: se optimiza el codigo del ejercicio 4

--- assets/JS/4_colores.js
+++ assets/JS/4_colores.js
@@ -1,4 +1,3 @@
-const btn = document.querySelector(".caja-color");
 const btnBlue = document.querySelector(".blue");
 const btnRed = document.querySelector(".red");
 const btnGreen = document.querySelector(".green");
@@ -8,48 +7,19 @@ let bgnBlack = false;
 
 
 
-btnBlue.addEventListener("click", ()=> {
-    if (bgnBlack) {
-        btnBlue.style.backgroundColor = "blue";
-        btnBlue.textContent = "1"
+function toggleBtn(element, color, text) {
+   if (element.style.backgroundColor === "black") {
+        element.style.backgroundColor = color;
+        element.style.color = "white";
+        element.textContent = text;
     } else {
-        btnBlue.style.backgroundColor = "black";
-        btnBlue.textContent = "BLACK"
+        element.style.backgroundColor = "black";
+        element.style.color = color === "yellow" ? "grey" : "white";
+        element.textContent = "BLACK";
     }
-    bgnBlack = !bgnBlack;
-});
+};
 
-btnRed.addEventListener("click", ()=> {
-    if (bgnBlack) {
-        btnRed.style.backgroundColor = "red";
-        btnRed.textContent = "2"
-    } else {
-        btnRed.style.backgroundColor = "black";
-        btnRed.textContent = "BLACK"
-    }
-    bgnBlack = !bgnBlack;
-});
-
-btnGreen.addEventListener("click", ()=> {
-    if (bgnBlack) {
-        btnGreen.style.backgroundColor = "green";
-        btnGreen.textContent = "3"
-    } else {
-        btnGreen.style.backgroundColor = "black";
-        btnGreen.textContent = "BLACK"
-    }
-    bgnBlack = !bgnBlack;
-});
-
-btnYellow.addEventListener("click", ()=> {
-    if (bgnBlack) {
-        btnYellow.style.backgroundColor = "yellow";
-        btnYellow.style.color = "black"
-        btnYellow.textContent = "4"
-    } else {
-        btnYellow.style.backgroundColor = "black";
-        btnYellow.textContent = "BLACK"
-        btnYellow.style.color = "white"
-    }
-    bgnBlack = !bgnBlack;
-});
+btnBlue.addEventListener("click", () => toggleBtn(btnBlue, "blue", "1"));
+btnRed.addEventListener("click", () => toggleBtn(btnRed, "red", "2"));
+btnGreen.addEventListener("click", () => toggleBtn(btnGreen, "green", "3"));
+btnYellow.addEventListener("click", () => toggleBtn(btnYellow, "yellow", "4"));
