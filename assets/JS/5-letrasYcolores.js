4e1b5f790c1f933d207c3f6bcff12631f98f12a9
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-23 09:54:41 -0400

    feature: se finaliza ejercicio 4-colores

---
+++ assets/JS/5-letrasYcolores.js
@@ -0,0 +1,37 @@
+const keyDown = document.querySelector(".key");
+const newDiv = document.querySelector("#new-div");
+
+document.addEventListener('keydown', function (event) {
+
+switch (event.key) {
+    case 'a':
+        keyDown.style.backgroundColor = "pink";
+        break;
+    case 's':
+        keyDown.style.backgroundColor = "orange";
+        break;
+    case 'd':
+        keyDown.style.backgroundColor = "cyan";
+        break;
+    case 'q':
+        newDiv.classList.add("new-key");
+        newDiv.style.backgroundColor = "violet";
+        newDiv.textContent = "New Div";
+        break;
+    case 'w':
+        newDiv.classList.add("new-key");
+        newDiv.style.backgroundColor = "grey";
+        newDiv.textContent = "New Div";
+        break;
+    case 'e':
+        newDiv.classList.add("new-key");
+        newDiv.style.backgroundColor = "brown";
+        newDiv.textContent = "New Div";
+        break;
+    default:
+        newDiv.style.backgroundColor = "white";
+}
+});
+
+
+
